e1550e228270df012fa914c08e339fbaadba1315
dschadd <dschadd@gmail.com>
2019-06-27 18:32:11 -0500

    add sign up and login functionality

--- src/router.js
+++ src/router.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import About from "./views/About.vue";
+import Signup from "./views/Signup.vue";
+import Login from "./views/Login.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
@@ -14,10 +17,17 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: About
+    },
+    {
+      path: "/signup",
+      name: "signup",
+      component: Signup
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
     }
   ]
-})
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>{{ consignees }}</h1>
   </div>
 </template>
 
@@ -8,13 +8,20 @@
 </style>
 
 <script>
+
+import axios from "axios";
+
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      consignees: "Welcome to Vue.js!"
     };
   },
-  created: function() {},
+  created: function() {
+    axios.get("http://localhost:3000/api/consignees").then(response => {
+      this.consignees = response.data;
+    });
+  },
   methods: {},
   computed: {}
 };
