0c147c9f38797b861a4a31abe904470ae30ba124
dschadd <dschadd@gmail.com>
2019-07-08 19:41:27 -0500

    abstract some data for ups request

--- src/views/Shipments/Create.vue
+++ src/views/Shipments/Create.vue
@@ -19,6 +19,41 @@ export default {
   data: function() {
     return {
       ups_rate: {},
+      shipper_address: {
+        address_line: "",
+        city: "",
+        state_code: "",
+        postal_code: "",
+        country_code: ""
+      },
+      ship_to_address: {
+        address_line: "",
+        city: "",
+        state_code: "",
+        postal_code: "",
+        country_code: ""
+      },
+      // This logic needs to be abstracted - it will live in the back end until I get 
+      // a basic rate request working for different addresses.
+      // "Package": {
+      //   "PackagingType": {
+      //     "Code": "02"
+      //   },
+      //   "Dimensions": {
+      //     "UnitOfMeasurement": {
+      //       "Code": "IN"
+      //     },
+      //     "Length": "5",
+      //     "Width": "4",
+      //     "Height": "3"
+      //   },
+      //   "PackageWeight": {
+      //     "UnitOfMeasurement": {
+      //       "Code": "Lbs",
+      //       "Description": "pounds"
+      //     },
+      //     "Weight": "1"
+      //   }
     };
   },
   created: function() {
